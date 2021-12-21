7d36f663099673e0a0a06fbbf7cbabb7918daef3
Max Vandervelden <70112222+mvdvldn03@users.noreply.github.com>
2021-12-20 23:49:41 -0500

    Update and rename get_string.py to datacollection/get_string.py

--- datacollection/get_string.py
+++ datacollection/get_string.py
@@ -6,11 +6,8 @@ import numpy as np
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--window-size=1920x1080")
-options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36")
-driver = webdriver.Chrome(options=options, executable_path='/Users/maxvandervelden/PycharmProjects/ihme_scraping/chromedriver 2')
-
-def switch():
-    print("hi")
+options.add_argument("user-agent=____")
+driver = webdriver.Chrome(options=options, executable_path='/chromedriver')
 
 def get_cases():
     x = driver.find_elements_by_xpath("//div[@class='_26ZypQtR8sLPiXEB1jzI1b']/*[name()='svg']")[2]
