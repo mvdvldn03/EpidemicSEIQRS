69e230a60a41803b9853963c0bf75ba98fccd7ba
Max Vandervelden <70112222+mvdvldn03@users.noreply.github.com>
2021-12-20 23:42:45 -0500

    Update MSELoss.m

--- MSELoss.m
+++ MSELoss.m
@@ -1,10 +1,9 @@
 function [MSE] = MSELoss(cases,model)
-%OPTIMIZE Summary of this function goes here
-%   Detailed explanation goes here
-a = zeros(1,length(cases));
-for i = 1:length(cases)
-    a(1, i) = (model(i)-cases(i))^2;
-end
-MSE = mean(a(1,:));
+    %Calculuates the mean squared error of case data relative to the predicted model data
+    a = zeros(1,length(cases));
+    for i = 1:length(cases)
+        a(1, i) = (model(i)-cases(i))^2;
+    end
+    MSE = mean(a(1,:));
 end
 
